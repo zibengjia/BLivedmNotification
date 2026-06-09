0fb4ef26ff9082421892a61e36cb741f8a8d15d5
zibengjia <zhaoyeguo@qq.com>
2026-06-09 22:45:05 +0800

    :bug:修复ToggleSwitch开启态悬停蓝色消失(WPF-UI缺失AccentFillColor Color资源),:sparkles:SC根据B站API价值自动设置持续时间和渐变背景色,:sparkles:新增弹幕/SC背景设置(颜色/透明度/圆角),:sparkles:SC对齐方式可配置

--- overlay-csharp/WpfOverlay/obj/Debug/net8.0-windows/WpfOverlay.AssemblyInfo.cs
+++ overlay-csharp/WpfOverlay/obj/Debug/net8.0-windows/WpfOverlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfOverlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccafa47898e15d8d9daef501f45c0180e185dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278605e3e13cf37d048026d96a2426d18a196c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfOverlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfOverlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
